3b6a997ca099c95a834de4386bde2de29c1d5f9c
dancer1325 <alfredotic0809@gmail.com>
2023-07-27 15:52:04 +0200

    feat(): add skeleton

---
+++ src/test/java/org/openwebinars/course/gettingStarted/HelloIT.java
@@ -0,0 +1,8 @@
+package org.openwebinars.course.gettingStarted;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloIT extends HelloTest {
+    // Execute the same tests but in packaged mode.
+}
