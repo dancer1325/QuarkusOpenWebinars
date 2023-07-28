d107cbe5639e7af52aa0ff8bc7b0c2d25863fe03
dancer1325 <alfredotic0809@gmail.com>
2023-07-28 12:31:34 +0200

    feat(): add quarkus configuration via eclipse.microprofile

--- src/main/java/org/openwebinars/course/gettingStarted/Hello.java
+++ src/main/java/org/openwebinars/course/gettingStarted/Hello.java
@@ -4,13 +4,18 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class Hello {
 
+    @ConfigProperty(name = "greetings.message")
+    String messageConfigured;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        //return "Hello from RESTEasy Reactive";
+        return messageConfigured;
     }
 }
